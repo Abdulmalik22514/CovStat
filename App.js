7a636e987a117972a0ec2f807ef231d8c3f79d01
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-05-27 15:02:00 +0100

    chore: refactor set up page

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import "react-native-gesture-handler";
 import React from "react";
 import Navigator from "./navigation";
+import MyTabs from "./tabNav";
 
 export default function App() {
-  return <Navigator />;
+  return <MyTabs />;
 }
