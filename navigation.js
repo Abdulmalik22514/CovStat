6039619d46243e7e9f071d21508a8b817fb0353b
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-05-27 15:02:24 +0100

    feat: added tab navigator

--- navigation.js
+++ navigation.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Onboarding from "./screens/Onboarding";
 import Covid19 from "./screens/covid19";
+import WorldMap from "./screens/worldStat";
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,8 @@ export default function Navigator() {
     <NavigationContainer>
       <Stack.Navigator headerMode={"none"}>
         <Stack.Screen name="onboarding" component={Onboarding} />
-        <Stack.Screen name="covid19" component={Covid19} />
+        <Stack.Screen name="Home" component={Covid19} />
+        <Stack.Screen name="Map" component={WorldMap} />
       </Stack.Navigator>
     </NavigationContainer>
   );
