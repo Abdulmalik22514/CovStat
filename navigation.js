ba5010bebbd6dae591381056106e088bcb01fef2
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-05-28 10:47:47 +0100

    chore: refactor navigation page

--- navigation.js
+++ navigation.js
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Onboarding from "./screens/Onboarding";
-import Covid19 from "./screens/covid19";
-import WorldMap from "./screens/worldStat";
+import Onboarding from "./screens/onboarding";
+import MyTabs from "./tabNav";
 
 const Stack = createStackNavigator();
 
@@ -12,8 +11,7 @@ export default function Navigator() {
     <NavigationContainer>
       <Stack.Navigator headerMode={"none"}>
         <Stack.Screen name="onboarding" component={Onboarding} />
-        <Stack.Screen name="Home" component={Covid19} />
-        <Stack.Screen name="Map" component={WorldMap} />
+        <Stack.Screen name="mytabs" component={MyTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
