c227e0ba9aef940a1bdb333380b9d709f517c6ee
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-05-28 10:47:06 +0100

    feat:  set up tab navigation

--- tabNav.js
+++ tabNav.js
@@ -1,44 +1,44 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import WorldMap from "./screens/worldStat";
-import Covid19 from "./screens/covid19";
-import { Ionicons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
+import WorldMap from "./screens/map";
+import Covid19 from "./screens/home";
 import { Home, Map } from "./assets/svg";
 import Education from "./screens/education";
 import ChinaStat from "./screens/chinaStat";
 
 const Tab = createBottomTabNavigator();
+const MapStack = createStackNavigator();
+
+function MapStackScreen() {
+  return (
+    <MapStack.Navigator headerMode={"none"}>
+      <MapStack.Screen name="Map" component={WorldMap} />
+      <MapStack.Screen name="China" component={ChinaStat} />
+    </MapStack.Navigator>
+  );
+}
 
 export default function MyTabs() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === "Home") {
-              iconName = focused ? <Home /> : "ios-information-circle-outline";
-            } else if (route.name === "Map") {
-              iconName = focused ? <Map /> : "ios-list";
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: "#FF647C",
-          inactiveTintColor: "#999999",
-        }}
-      >
-        <Tab.Screen name="China" component={ChinaStat} />
-        <Tab.Screen name="Home" component={Covid19} />
-        <Tab.Screen name="Map" component={WorldMap} />
-        <Tab.Screen name="Education" component={Education} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return <Home color={focused ? "#FF647C" : "#999999"} />;
+          } else if (route.name === "Map") {
+            return <Map color={focused ? "#FF647C" : "#999999"} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#FF647C",
+        inactiveTintColor: "#999999",
+      }}
+    >
+      <Tab.Screen name="Home" component={Covid19} />
+      <Tab.Screen name="Map" component={MapStackScreen} />
+      <Tab.Screen name="Education" component={Education} />
+    </Tab.Navigator>
   );
 }
